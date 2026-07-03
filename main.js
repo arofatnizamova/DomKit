26c25d5ca611808e50f5ed30817d036999057969
Your Name <you@example.com>
2026-07-03 12:24:34 +0500

    feat: add pages

--- main.js
+++ main.js
@@ -1,50 +1,7 @@
-$(document).ready(function() {
-    $('.slick-slider').each(function() {
-        let slider = $(this);
+import initSwiper from './initSwiper.js';
+import initSlick from './initSlick.js';
 
-        let options = {
-            prevArrow: slider.parent().find('.slider-prev'),
-            nextArrow: slider.parent().find('.slider-next'),
-            infinite: true,
-            autoplay: true,
-            dots: false
-        };
-
-        let extraOptions = {};
-
-        if (slider.hasClass('services')) {
-            extraOptions = {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                centerMode: false,
-                arrows: true,
-                dots: false,
-                autoplay: true,
-                responsive: [{
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 2.5,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 2.3,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 576,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                        }
-                    },
-                ]
-            };
-        }
-
-        slider.slick($.extend({}, options, extraOptions));
-    });
+$(document).ready(function () {
+    initSwiper();
+    initSlick($);
 });
